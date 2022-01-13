a05a8be052e34725307cb9192f9b9a2bbd01d0bb
RowdyFlite <rowdyflite@gmail.com>
2022-01-13 13:39:54 -0800

    Start progress on moving logic into js.
    Player is not yet included in js.

---
+++ story/src/js/combat.js
@@ -0,0 +1,38 @@
+window.start_combat = function(){
+	let combat = {};
+	combat.playerTeam = [];
+	combat.opponents = [];
+
+	combat.attack = function(defender, damage){
+		defender.hp = Math.max(0, defender.hp - damage);
+	};
+
+	combat.tick = function(){
+		let playerAlive = false;
+		let opponentAlive = false;
+		for (fighter in this.playerTeam){
+			if (fighter.hp >= 0){
+				playerAlive = true;
+			}
+		}
+		for (opponent in this.playerTeam){
+			if (opponent.hp >= 0){
+				opponentAlive = true;
+			}
+		}
+
+		this.finished = !playerAlive || !opponentAlive;
+		if (this.finished){
+			this.won = playerAlive && !opponentAlive;
+			this.lost = !this.won;
+		}
+	};
+
+	combat.finished = false;
+	combat.won = false;
+	combat.lost = false;
+	return combat;
+}
+
+let combat = window.start_combat([], []);
+combat.tick();
