542eff489e0fa00c613d074a52e11d493e1c4288
RowdyFlite <rowdyflite@gmail.com>
2022-01-13 19:28:54 -0800

    Fix broken for loops.

--- story/src/js/combat.js
+++ story/src/js/combat.js
@@ -4,28 +4,33 @@ window.start_combat = function(){
 	combat.opponents = [];
 
 	combat.attack = function(defender, damage){
-		window.alert("Attacking defender for " + damage + " damage!");
 		defender.hp = Math.max(0, defender.hp - damage);
 	};
 
 	combat.tick = function(){
 		let playerAlive = false;
 		let opponentAlive = false;
-		for (let player in this.playerTeam){
+		this.playerTeam.forEach(player => {
 			if (player.hp >= 0){
 				playerAlive = true;
 			}
-		}
-		for (let opponent in this.opponents){
+		});
+
+		this.opponents.forEach(opponent => {
 			if (opponent.hp >= 0){
 				opponentAlive = true;
 			}
-		}
+		});
 
-		this.finished = !playerAlive || !opponentAlive;
-		if (this.finished){
-			this.won = playerAlive && !opponentAlive;
-			this.lost = !this.won;
+		if (!playerAlive){
+			this.finished = true;
+			this.won = false;
+			this.lost = true;
+		}
+		else if (!opponentAlive){
+			this.finished = true;
+			this.won = false;
+			this.lost = true;	
 		}
 	};
 
@@ -34,6 +39,3 @@ window.start_combat = function(){
 	combat.lost = false;
 	return combat;
 }
-
-let combat = window.start_combat([], []);
-combat.tick();
