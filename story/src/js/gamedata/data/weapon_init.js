2a000825b231e31e53ee219d569b58070c144b74
RowdyFlite <rowdyflite@gmail.com>
2022-01-25 20:45:53 -0800

    Move init for gamedata into its own section.

---
+++ story/src/js/gamedata/data/weapon_init.js
@@ -0,0 +1,21 @@
+setup.init_weapons = function(){
+    const WEAPON = "Weapon";
+	setup.gamedata.add(WEAPON, {
+		id: "dagger",
+		cost: 3,
+		att: 1,
+		name: "Dagger"
+	});
+	setup.gamedata.add(WEAPON, {
+		id: "shortsword",
+		cost: 10,
+		att: 3,
+		name: "Short Sword"
+	});
+	setup.gamedata.add(WEAPON, {
+		id: "longsword",
+		cost: 25,
+		att: 5,
+		name: "Long Sword"
+	});
+}
