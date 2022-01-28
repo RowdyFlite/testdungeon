915f5dfcbd9bd1770cc5c21335372d7876c2daf1
RowdyFlite <rowdyflite@gmail.com>
2022-01-27 22:36:16 -0800

    Can navigate floors again!

--- story/src/js/gamedata/data/floors_init.js
+++ story/src/js/gamedata/data/floors_init.js
@@ -1,11 +1,25 @@
 setup.init_floors = function(){
-    const FLOOR = "Floor";
-    setup.gamedata.add(FLOOR, {
-		id: "1",
-        encounter_table: null,
-        events: {},
-        encounters: {},
-        encounter_rate: 0.5,
-	});
+  const FLOOR = "Floor";
 
+  setup.gamedata.add(FLOOR, {
+    id: "1",
+    encounter_table: null,
+    events: {},
+    encounters: {},
+    encounter_rate: 0.5,
+  });
+  setup.gamedata.add(FLOOR, {
+    id: "2",
+    encounter_table: null,
+    events: {},
+    encounters: {},
+    encounter_rate: 0.5,
+  });
+  setup.gamedata.add(FLOOR, {
+    id: "3",
+    encounter_table: null,
+    events: {},
+    encounters: {},
+    encounter_rate: 0.5,
+  });
 }
--- story/src/js/gamedata/data/rooms_init.js
+++ story/src/js/gamedata/data/rooms_init.js
@@ -15,8 +15,8 @@ setup.init_rooms = function(){
     create_exit(setup.gamedata.get(ROOM, "1-0,0"), setup.gamedata.get(ROOM, "1-0,1"));
     create_exit(setup.gamedata.get(ROOM, "1-0,1"), setup.gamedata.get(ROOM, "1-0,2"));
 
-    setup.gamedata.get(ROOM, "1-0,0").DOWN = OUT;
-    setup.gamedata.get(ROOM, "1-0,2").UP = "2-0,0";
+    setup.gamedata.get(ROOM, "1-0,0").exits[DOWN] = OUT;
+    setup.gamedata.get(ROOM, "1-0,2").exits[UP] = "2-0,0";
 
     setup.gamedata.add(ROOM, create_room(2, 0, 0));
     setup.gamedata.add(ROOM, create_room(2, 0, 1));
@@ -29,8 +29,8 @@ setup.init_rooms = function(){
     create_exit(setup.gamedata.get(ROOM, "2-0,1"), setup.gamedata.get(ROOM, "2-1,1"));
     create_exit(setup.gamedata.get(ROOM, "2-0,1"), setup.gamedata.get(ROOM, "2--1,1"));
 
-    setup.gamedata.get(ROOM, "2-0,0").DOWN = "1-0,2";
-    setup.gamedata.get(ROOM, "2-0,2").UP = "3-0,0";
+    setup.gamedata.get(ROOM, "2-0,0").exits[DOWN] = "1-0,2";
+    setup.gamedata.get(ROOM, "2-0,2").exits[UP] = "3-0,0";
 
     setup.gamedata.add(ROOM, create_room(3, 0, 0));
     setup.gamedata.add(ROOM, create_room(3, 1, 0));
@@ -49,7 +49,7 @@ setup.init_rooms = function(){
     create_exit(setup.gamedata.get(ROOM, "3-3,-2"), setup.gamedata.get(ROOM, "3-2,-2"));
     create_exit(setup.gamedata.get(ROOM, "3-2,-2"), setup.gamedata.get(ROOM, "3-2,-1"));
     
-    setup.gamedata.get(ROOM, "3-0,0").DOWN = "2-0,2";
+    setup.gamedata.get(ROOM, "3-0,0").exits[DOWN] = "2-0,2";
 }
 
 function create_room(floor, x, y){
