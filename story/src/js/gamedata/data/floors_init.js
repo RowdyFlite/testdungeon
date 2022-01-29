badaae41fb9d2f65651475c2ae30b1474cd289ba
RowdyFlite <rowdyflite@gmail.com>
2022-01-28 19:23:22 -0800

    Fix events. Still not happy with how they're done.

--- story/src/js/gamedata/data/floors_init.js
+++ story/src/js/gamedata/data/floors_init.js
@@ -5,19 +5,36 @@ setup.init_floors = function(){
     id: "1",
     encounter_table: "rats",
     encounter_chance: 1,
-    rooms: new Set
+    rooms: new Set,
+    events: {
+      "1:0,0": {
+        id: "TestEventOnce",
+        type: "once", //once, always
+        passage:"TestEventOnce",
+        closing_text: "You cleared it!",
+      }
+    }
   });
   setup.gamedata.add(FLOOR, {
     id: "2",
     encounter_table: "ratsgobbo",
     encounter_chance: 0.5,
-    rooms: new Set
+    rooms: new Set,
+    events: {}
   });
   setup.gamedata.add(FLOOR, {
     id: "3",
     encounter_table: "goblins",
     encounter_chance: 0.5,
-    rooms: new Set
+    rooms: new Set,
+    events: {
+      "3:2,-1":{
+        id: "TestEventAlways",
+        type: "always", //once, always
+        passage:"TestEventAlways",
+        closing_text: "You cleared it!",
+      }
+    }
   });
 }
 
